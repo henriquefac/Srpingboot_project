7e81737615c74e401728f82c18308b30953c7aca
henriquefac <henrique.fdut@gmail.com>
2024-02-17 12:41:06 -0300

    Projeto SpringBoot (primeiro commit)

---
+++ demo/src/main/java/com/testefirst/demo/controler/ProductControler.java
@@ -0,0 +1,8 @@
+package com.testefirst.demo.controler;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductControler {
+    
+}
---
+++ demo/src/main/java/com/testefirst/demo/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.testefirst.demo.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductRepository, UUID>{
+    
+}
